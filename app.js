3afff33a47ba6f3d7fa45a1344866d5ddbe94241
Fe! <cp176823@gmail.com>
2025-04-23 00:22:01 +0000

    Update index.html and app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ async function encryptMessage() {
     alert("Please provide a public key.");
     return;
   }
-  
+
   try {
     const publicKey = await openpgp.readKey({ armoredKey: publicKeyArmored });
     // Encrypt the message using the public key
@@ -17,7 +17,6 @@ async function encryptMessage() {
 
     // Append the public key to the encrypted message
     const result = `${encryptedMessage}\n`;
-  
     document.getElementById("encrypted-message").value = result;
     } catch (error) {
     alert("Error encrypting message: " + error);
@@ -32,27 +31,27 @@ async function decryptMessage() {
     alert("Please provide your private key passphrase.");
     return;
   }
-    
+
   try {
     // Split the encrypted message to extract the encrypted content
     const messageParts = encryptedMessageWithKey.split("-----BEGIN PGP PUBLIC KEY BLOCK-----");
     const encryptedMessage = messageParts[0].trim();
-    
+
     // Read and decrypt the private key
     const privateKey = await openpgp.readPrivateKey({ armoredKey: privateKeyArmored });
     const decryptedPrivateKey = await openpgp.decryptKey({
       privateKey,
       passphrase: privateKeyPassphrase
     });
-    
+
     // Decrypt the message using the private key
     const message = await openpgp.decrypt({
       message: await openpgp.readMessage({ armoredMessage: encryptedMessage }),
       decryptionKeys: decryptedPrivateKey
     });
-    
+
     document.getElementById("decrypted-message").value = message.data; // Use `message.data` for the decrypted text
-  } 
+  }
   catch (error) {
     alert("Error decrypting message: " + error);
   }
